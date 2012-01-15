5467d8c76ec9cdaaaf274a2d96a2ec4e21f3625d
Will O'Brien <will.obrien@dibit.co>
2012-01-15 08:01:45 -0500

    Custom exception resolver to send Http 422 error when message is properly formed but unreadable

---
+++ src/test/todo/json/schema/spring/TodoHandlerExceptionResolverTest.java
@@ -0,0 +1,39 @@
+package todo.json.schema.spring;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import todo.hibernate.entities.User;
+import todo.json.schema.spring.HttpMessageUnprocessableException;
+
+import java.lang.Object;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: Will O'Brien
+ * Date: 1/14/12
+ * Time: 4:55 PM
+ */
+public class TodoHandlerExceptionResolverTest {
+
+    private TodoHandlerExceptionResolver exceptionResolver;
+
+    @Before
+    public void setUp() throws Exception {
+        exceptionResolver = new TodoHandlerExceptionResolver();
+    }
+
+    @Test
+    public void testResolveException() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        MockHttpServletResponse resp = new MockHttpServletResponse();
+        Exception ex = new HttpMessageUnprocessableException("test");
+        Object handler = new User();
+
+        exceptionResolver.resolveException(req, resp, handler, ex);
+
+        assertTrue(resp.getStatus() == 422);
+    }
+}
